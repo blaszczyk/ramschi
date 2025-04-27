6b767fcb2c99f110dc8b77d85a5f937f2aaad5d9
Michael Blaszczyk <michael.i.blaszczyk@gmail.com>
2025-04-27 15:33:21 +0200

    ramschi server with db schema

---
+++ ramschi-server/src/main/java/com/github/blaszczyk/ramschi/ramschi_server/RamschiServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.blaszczyk.ramschi.ramschi_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamschiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamschiServerApplication.class, args);
+	}
+
+}
