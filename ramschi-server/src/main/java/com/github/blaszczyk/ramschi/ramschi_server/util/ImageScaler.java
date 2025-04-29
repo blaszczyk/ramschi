51361ff1cd2ff25554b25c5c1609f472d8f96278
Michael Blaszczyk <michael.i.blaszczyk@gmail.com>
2025-04-29 13:03:39 +0200

    first scale then rotate then scale

--- ramschi-server/src/main/java/com/github/blaszczyk/ramschi/ramschi_server/util/ImageScaler.java
+++ ramschi-server/src/main/java/com/github/blaszczyk/ramschi/ramschi_server/util/ImageScaler.java
@@ -30,53 +30,71 @@ public class ImageScaler {
     private int previewWidth;
 
     public void addThumbnailAndPreview(ImageEntity entity) {
-        final BufferedImage image = getOrientedImage(entity);
 
-        final int thumbnailWidth = thumbnailHeight * image.getWidth() / image.getHeight();
-        final byte[] thumbnail = scale(image, thumbnailWidth, thumbnailHeight);
-        entity.setThumbnail(thumbnail);
+        final BufferedImage image = getImage(entity);
+
+        final Metadata metadata = getMetaData(entity);
 
         final int previewHeight = previewWidth * image.getHeight() / image.getWidth();
-        final byte[] preview = scale(image, previewWidth, previewHeight);
-        entity.setPreview(preview);
-    }   
+        final BufferedImage tempPreview = buffered(image.getScaledInstance(previewWidth, previewHeight, Image.SCALE_SMOOTH));
 
-    private byte[] scale(BufferedImage image, int width, int height) {
-        final Image scaledImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-        final BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        bufferedImage.getGraphics().drawImage(scaledImage, 0, 0, null);
-        try(final ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-            ImageIO.write(bufferedImage, "PNG", outputStream);
-            return outputStream.toByteArray();
+        final BufferedImage preview = fixOrientation(tempPreview, metadata);
+        entity.setPreview(getBytes(preview));
+
+        final int thumbnailWidth = thumbnailHeight * image.getWidth() / image.getHeight();
+        final BufferedImage thumbnail = buffered(preview.getScaledInstance(thumbnailWidth, thumbnailHeight, Image.SCALE_SMOOTH));
+        entity.setThumbnail(getBytes(thumbnail));
+    }
+
+    private BufferedImage getImage(ImageEntity entity) {
+        try (final InputStream inputStream = new ByteArrayInputStream(entity.getOriginal())) {
+            return ImageIO.read(inputStream);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private BufferedImage getOrientedImage(ImageEntity entity) {
-        try(final InputStream dataIS = new ByteArrayInputStream(entity.getOriginal())) {
-            final BufferedImage badImage = ImageIO.read(dataIS);
-            try(final InputStream metadataIS = new ByteArrayInputStream(entity.getOriginal())) {
-                final Metadata metadata = ImageMetadataReader.readMetadata(metadataIS);
-                final var exifIFDirectory = metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
-                if (exifIFDirectory == null) {
-                    return badImage;
-                }
-                final int orientation = exifIFDirectory.getInt(ExifIFD0Directory.TAG_ORIENTATION);
-                return switch (orientation) {
-                    case 1 -> badImage;
-                    case 2 -> Scalr.rotate(badImage, Scalr.Rotation.FLIP_HORZ);
-                    case 3 -> Scalr.rotate(badImage, Rotation.CW_180);
-                    case 4 -> Scalr.rotate(badImage, Rotation.FLIP_VERT);
-                    case 5 -> Scalr.rotate(Scalr.rotate(badImage, Rotation.CW_90), Rotation.FLIP_HORZ);
-                    case 6 -> Scalr.rotate(badImage, Rotation.CW_90);
-                    case 7 -> Scalr.rotate(Scalr.rotate(badImage, Rotation.CW_90), Rotation.FLIP_VERT);
-                    case 8 -> Scalr.rotate(badImage, Rotation.CW_270);
-                    default -> badImage;
-                };
-            } catch (ImageProcessingException | MetadataException e) {
-                throw new RuntimeException(e);
-            }
+    private Metadata getMetaData(ImageEntity entity) {
+        try (final InputStream inputStream = new ByteArrayInputStream(entity.getOriginal())) {
+            return ImageMetadataReader.readMetadata(inputStream);
+        } catch (IOException | ImageProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private BufferedImage fixOrientation(BufferedImage image, Metadata metadata) {
+        final var exifIFDirectory = metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
+        if (exifIFDirectory == null) {
+            return image;
+        }
+        try {
+            final int orientation = exifIFDirectory.getInt(ExifIFD0Directory.TAG_ORIENTATION);
+            return switch (orientation) {
+                case 1 -> image;
+                case 2 -> Scalr.rotate(image, Scalr.Rotation.FLIP_HORZ);
+                case 3 -> Scalr.rotate(image, Rotation.CW_180);
+                case 4 -> Scalr.rotate(image, Rotation.FLIP_VERT);
+                case 5 -> Scalr.rotate(Scalr.rotate(image, Rotation.CW_90), Rotation.FLIP_HORZ);
+                case 6 -> Scalr.rotate(image, Rotation.CW_90);
+                case 7 -> Scalr.rotate(Scalr.rotate(image, Rotation.CW_90), Rotation.FLIP_VERT);
+                case 8 -> Scalr.rotate(image, Rotation.CW_270);
+                default -> image;
+            };
+        } catch (MetadataException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private BufferedImage buffered(Image image) {
+        final BufferedImage bufferedImage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_RGB);
+        bufferedImage.getGraphics().drawImage(image, 0, 0, null);
+        return bufferedImage;
+    }
+
+    private byte[] getBytes(BufferedImage image) {
+        try(final ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            ImageIO.write(image, "PNG", outputStream);
+            return outputStream.toByteArray();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
