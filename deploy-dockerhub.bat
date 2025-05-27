fc07ff3c0b66105500e09d36052562343d7c2a95
Michael Blaszczyk <michael.i.blaszczyk@gmail.com>
2025-05-27 20:09:24 +0200

    1.2.13

--- deploy-dockerhub.bat
+++ deploy-dockerhub.bat
@@ -1,6 +1,6 @@
 set JAVA_HOME="c:\tools\java\openlogic-openjdk-17.0.15+6-windows-x64\"
 
-set RAMSCHI_VERSION=1.2.12
+set RAMSCHI_VERSION=1.2.13
 set IMAGE_TAG=blaszczyk/ramschi:%RAMSCHI_VERSION%
 
 cd ramschi-ui
