0176ef6b83f73d025736aca01ab9cb6e3fb7ecb4
Michael Blaszczyk <michael.i.blaszczyk@gmail.com>
2025-06-07 17:47:07 +0200

    Counter util class

--- ramschi-server/src/main/java/com/github/blaszczyk/ramschi/ramschi_server/service/AssigneeService.java
+++ ramschi-server/src/main/java/com/github/blaszczyk/ramschi/ramschi_server/service/AssigneeService.java
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
-import java.util.Collection;
 import java.util.List;
 
+import static com.github.blaszczyk.ramschi.ramschi_server.util.Counter.countByProperty;
 import static reactor.core.publisher.Mono.zip;
 
 @Service
@@ -36,20 +36,20 @@ public class AssigneeService {
     public Mono<List<Assignee>> getAllAssignees() {
         final var fetchAssignees = assigneeRepository.findAllOrderByName().collectList();
         final var fetchItems = itemAssigneeRepository.findAll()
-                .collectMultimap(ItemAssigneeEntity::getAssignee);
+                .collect(countByProperty(ItemAssigneeEntity::getAssignee));
         final var fetchComments = commentRepository.findAll()
-                .collectMultimap(CommentEntity::getAuthor);
+                .collect(countByProperty(CommentEntity::getAuthor));
         return zip(fetchAssignees, fetchItems, fetchComments).map(tuple -> {
                     final var entities = tuple.getT1();
-                    final var items = tuple.getT2();
-                    final var comments = tuple.getT3();
+                    final var itemCount = tuple.getT2();
+                    final var commentCount = tuple.getT3();
                     return entities.stream().map(entity -> {
                         final String name = entity.getName();
                         final byte[] passwordSHA256 = entity.getPasswordSHA256();
                         final boolean secure = passwordSHA256 != null && passwordSHA256.length > 0;
-                        final int itemCount = count(items.get(name));
-                        final int commentCount = count(comments.get(name));
-                        return new Assignee(name, entity.getRole(), secure, itemCount, commentCount);
+                        return new Assignee(name, entity.getRole(), secure,
+                                itemCount.get(name),
+                                commentCount.get(name));
                     }).toList();
                 });
     }
@@ -69,7 +69,4 @@ public class AssigneeService {
                 .then();
     }
 
-    private static int count(Collection<?> collection) {
-        return collection == null ? 0 : collection.size();
-    }
 }
